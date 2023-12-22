a183516b17c8908b68b4fd9b9a84197e77f542be
Mark Khramov <hramov.mark321@gmail.com>
2023-12-22 18:07:31 +0300

    added i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
 
+
+const Component = () => {
+    const {t, i18n} = useTranslation()
+    const toggle = () => {
+        return i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
+    return (
+
+        <div>
+            <button onClick={toggle}>{t('Перевод')}</button>
+            {t('Тестовый пример')}
+        </div>
+    )
+}
 
 
 const App = () => {
-   const { theme } = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar/>
-                <AppRouter />
-            </div>
-
+            <Suspense fallback=''>
+                <Navbar/>
+                <Component/>
+                <div className='content-page'>
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
